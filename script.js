d212695181808762b2749be9ecf7d6da9952ce9e
Saqreed <101287632+saqreed@users.noreply.github.com>
2024-09-21 09:28:07 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#logo-link').addEventListener('click', function(event) {
+        event.preventDefault(); // Предотвращаем стандартное поведение ссылки
+        document.querySelector('#top').scrollIntoView({ behavior: 'smooth' }); // Плавный скролл
+    });
+});
+*/
