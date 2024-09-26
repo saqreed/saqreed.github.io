414b89ce27815e6ebfd8b0e5b746d1818314b19e
Saqreed <101287632+saqreed@users.noreply.github.com>
2024-09-27 00:05:49 +0400

    I played a little with the colors of the backdrop

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-/*
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#logo-link').addEventListener('click', function(event) {
         event.preventDefault(); // Предотвращаем стандартное поведение ссылки
         document.querySelector('#top').scrollIntoView({ behavior: 'smooth' }); // Плавный скролл
     });
 });
-*/
+
